0aad6c0fa797a272c3285914c781e0e660a62b3e
esther-h-li <estherhuanneli@gmail.com>
2025-10-13 18:25:48 -0400

    Improved the home page, added button and image components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,21 @@
+import Button from "@/components/button";
+import ImageViewer from "@/components/ImageViewer";
 import { StyleSheet, Text, View } from "react-native";
+const mascot = require("../../assets/images/reading.png")
 
 export default function index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Academic planner is...!</Text>
+        <View style={styles.box}>
+        <ImageViewer imgSource = {mascot}/>
+        <Text style={styles.text}>"The best time to start was 20 years ago. The second best time is now."{"\n"}Academic Planner is here to help you towards your goals!</Text>
+        </View>
+      
+      
+      <View style = {styles.box}>
+        <Button label = "Ask me anything!"/>
+        <Button label = "Check your progress!"/>
+      </View>
     </View>
   );
 }
@@ -14,13 +26,20 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     color: "#EAE7E2",
+    margin: 18,
+    flexDirection: "column",
+  },
+  box: {
+    flex:1,
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    width:500,
+    height:200,
+    color: "#eef4fb",
   },
   text: {
     color: "#27472D",
   },
-  button : {
-      fontSize: 10,
-      textDecorationLine: "underline",
-      color: "#317873",
-  }
+  
+ 
 })
